a45251ef22fb29043709e2c8c09502c294d34063
Denys-Nalyvaiko <nalyvaikodenys@gmail.com>
2023-10-12 15:42:44 +0200

    added html head

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-
-echo "Hello there!";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Student Dashboard</title>
+</head>
+<body>
+  <h1>Student Dashboard project</h1>
+  <?php
+  echo "Hello there!";
+  ?>
+</body>
+</html>

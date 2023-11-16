0b89e953e798b78b8b527a764eb10426b162216a
Denys-Nalyvaiko <nalyvaikodenys@gmail.com>
2023-11-16 15:15:14 +0100

    add routing

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Student Dashboard</title>
-</head>
-<body>
-  <h1>Student Dashboard project</h1>
-  <?php
-  echo "Hello there!";
-  ?>
-</body>
-</html>
+<?php
+
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('index', 'DefaultController');
+Routing::get('projects', 'DefaultController');
+Routing::run($path);

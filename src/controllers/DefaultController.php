734d7a6b454f2ddc229c12ac683eaef299b90bf5
Denys-Nalyvaiko <nalyvaikodenys@gmail.com>
2023-11-23 15:54:44 +0100

    add basic authorization

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -5,12 +5,10 @@ require_once 'AppController.php';
 class DefaultController extends AppController{
 
   public function index(){
-    // TODO diplay login.html
     $this->render('login');
   }
 
   public function projects(){
-    // TODO display projects.html
     $this->render('projects');
   }
 }

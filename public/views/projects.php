2f87a80f8de2e43c76a27f2c86f83bbda61d9ed0
Denys-Nalyvaiko <nalyvaikodenys@gmail.com>
2023-12-07 15:42:01 +0100

    configure add projects controller

--- public/views/projects.php
+++ public/views/projects.php
@@ -1,13 +1,60 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Student Dahsboard</title>
-    <link rel="stylesheet" href="public/css/style.css" />
-    <link rel="stylesheet" href="public/css/projects.css" />
-  </head>
-  <body>
-    <h1>Projects</h1>
-  </body>
-</html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="public/css/style.css">
+    <link rel="stylesheet" type="text/css" href="public/css/projects.css">
+
+    <script src="https://kit.fontawesome.com/723297a893.js" crossorigin="anonymous"></script>
+    <title>PROJECTS</title>
+</head>
+
+<body>
+    <div class="base-container">
+        <nav>
+            <img src="public/img/logo.svg">
+            <ul>
+                <li>
+                    <i class="fas fa-project-diagram"></i>
+                    <a href="#" class="button">projects</a>
+                </li>
+                <li>
+                    <i class="fas fa-project-diagram"></i>
+                    <a href="#" class="button">projects</a>
+                </li>
+                <li>
+                    <i class="fas fa-project-diagram"></i>
+                    <a href="#" class="button">projects</a>
+                </li>
+                <li>
+                    <i class="fas fa-project-diagram"></i>
+                    <a href="#" class="button">projects</a>
+                </li>
+            </ul>
+        </nav>
+        <main>
+            <header>
+                <div class="search-bar">
+                    <form>
+                        <input placeholder="search project">
+                    </form>
+                </div>
+                <div class="add-project">
+                    <i class="fas fa-plus"></i> add project
+                </div>
+            </header>
+            <section class="projects">
+                <div id="project-1">
+                    <img src="public/uploads/<?= $project->getImage() ?>">
+                    <div>
+                        <h2><?= $project->getTitle() ?></h2>
+                        <p><?= $project->getDescription() ?></p>
+                        <div class="social-section">
+                            <i class="fas fa-heart"> 600</i>
+                            <i class="fas fa-minus-square"> 121</i>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+    </div>
+</body>
